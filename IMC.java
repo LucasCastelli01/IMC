444f8259e3d39aecff24e9b24b8dd3e670e4feaf
Lucas <1139217@atitus.edu.br>
2026-09-02 21:34:02 -0300

    Base sistema IMC

---
+++ IMC.java
@@ -0,0 +1,18 @@
+void main(){
+
+    char genero = IO.readln("Qual seu gênero? M - masculino F - feminino N - prefiro não dizer").charAt(0);
+
+
+    switch (genero)) {
+        case 'M', 'm':
+            if (imc < 20)
+                IO.println("Abaixo do normal");
+            else if(imc < 25)
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}
