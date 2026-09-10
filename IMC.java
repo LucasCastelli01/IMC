66b72cea7400a945a1999e0a09a3d69e1165cf1f
Lucas Castelli <1139217@atitus.edu.br>
2026-09-10 08:40:57 -0300

    feito classificação diferente

--- IMC.java
+++ IMC.java
@@ -9,48 +9,53 @@ void main(){
     double peso = Double.valueOf (pesoString);
 
     double imc = (peso /  (Math.pow(altura, 2)));
-    System.out.printf ("Seu imc é de: %.2f ", imc);
-    IO.println("Classificado como:");
 
-    
+
+    String classificacao = "";
+
     switch (genero) {
         case 'M', 'm':
             if (imc < 20)
-                IO.println("Abaixo do normal");
+                classificacao = "Abaixo do normal";
         
             else if(imc >= 20 && imc <= 24.9)
-                IO.println("Normal");
+                classificacao = "Normal";
 
             else if(imc >= 25 && imc <= 29.9)
-                IO.println("Obesidade Leve");
+                classificacao = "Obesidade Leve";
 
             else if(imc >= 30 && imc <= 39.9)
-                IO.println("Obesidade Moderada");
+                classificacao = "Obesidade Moderada";
 
             else if(imc >= 40)
-                IO.println("Obesidade Mórbida");
+                classificacao = "Obesidade Mórbida";
 
             break;
         case 'F', 'f', 'N', 'n':
             if (imc < 19)
-                IO.println("Abaixo do normal");
+                classificacao = "Abaixo do normal";
         
             else if(imc >= 19 && imc <= 23.9)
-                IO.println("Normal");
+                classificacao = "Normal";
 
             else if(imc >= 24 && imc <= 28.9)
-                IO.println("Obesidade Leve");
+                classificacao = "Obesidade Leve";
 
             else if(imc >= 29 && imc <= 38.9)
-                IO.println("Obesidade Moderada");
+                classificacao = "Obesidade Moderada";
 
             else if(imc >= 39)
-                IO.println("Obesidade Mórbida");
+                classificacao = "Obesidade Mórbida";
 
             break;
         default:
             IO.println("Gênero inválido.");
-            break;
+            break;    
     }
+
+
+    
+    
+
 
 }
